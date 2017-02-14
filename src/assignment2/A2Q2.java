bd8725160122f8b41fc063027d97f03d47642e55
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-02-14 10:10:46 -0500

    Initial Commit

---
+++ src/assignment2/A2Q2.java
@@ -0,0 +1,20 @@
+package assignment2;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author laveh2107
+ */
+public class A2Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
