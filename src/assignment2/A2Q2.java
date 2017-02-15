c233587b17d74ee0e50f0f9b50bb94d04f3aaa39
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-02-15 09:45:49 -0500

    Initial Commit

--- src/assignment2/A2Q2.java
+++ src/assignment2/A2Q2.java
@@ -10,7 +10,6 @@ import becker.robots.Wall;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author laveh2107
@@ -23,10 +22,10 @@ public class A2Q2 {
     public static void main(String[] args) {
         // Create City
         City kw = new City();
-        
+
         // Create Bot
         Robot ay = new Robot(kw, 1, 0, Direction.EAST);
-        
+
         //Creating Track
         new Wall(kw, 1, 0, Direction.SOUTH);
         new Wall(kw, 1, 1, Direction.SOUTH);
@@ -37,46 +36,46 @@ public class A2Q2 {
         new Wall(kw, 1, 6, Direction.SOUTH);
         new Wall(kw, 1, 7, Direction.SOUTH);
         new Wall(kw, 1, 8, Direction.SOUTH);
-        
+
         //create hurdle
         new Wall(kw, 1, 0, Direction.EAST);
         new Wall(kw, 1, 1, Direction.EAST);
         new Wall(kw, 1, 3, Direction.EAST);
         new Wall(kw, 1, 6, Direction.EAST);
-        
+
         //Create thing
         new Thing(kw, 1, 8);
-        
+
         //Move to end hurdles
-        
-        while(true){
-        //Check if it can move
-        //yes, move, no, turn over hurdle
-        //end if can pick up
-        //Check if bot can move
-        while (!ay.canPickThing()){
-        //Check if front is clear
-        while (ay.frontIsClear()){
-        //Move if clear
-        ay.move();
+
+        while (true) {
+            //Check if it can move
+            //yes, move, no, turn over hurdle
+            //end if can pick up
+            //Check if bot can move
+            //while (!ay.canPickThing()) {
+                //Check if front is clear
+                while (ay.frontIsClear()) {
+                    //Move if clear
+                    ay.move();
+                //}
+                //Jump over hurdle
+                ay.turnLeft();
+                ay.move();
+                ay.turnLeft();
+                ay.turnLeft();
+                ay.turnLeft();
+                ay.move();
+                ay.turnLeft();
+                ay.turnLeft();
+                ay.turnLeft();
+                ay.move();
+                ay.turnLeft();
+            }
+            //Stop once the bot is on the bot
+            break;
         }
-        //Jump over hurdle
-        ay.turnLeft();
-        ay.move();
-        ay.turnLeft();
-        ay.turnLeft();
-        ay.turnLeft();
-        ay.move();
-        ay.turnLeft();
-        ay.turnLeft();
-        ay.turnLeft();
-        ay.move();
-        ay.turnLeft();
-        }
-        //Stop once the bot is on the bot
-        break;
-        }
-        
-      
+
+
     }
 }
