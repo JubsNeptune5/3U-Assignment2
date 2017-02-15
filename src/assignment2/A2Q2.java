9960e674162e858588a174c3f2c15713fc68b181
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-02-15 09:39:03 -0500

    Initial Commit

--- src/assignment2/A2Q2.java
+++ src/assignment2/A2Q2.java
@@ -4,6 +4,7 @@ import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
 import becker.robots.Thing;
+import becker.robots.Wall;
 
 /*
  * To change this template, choose Tools | Templates
@@ -24,9 +25,58 @@ public class A2Q2 {
         City kw = new City();
         
         // Create Bot
-        Robot ay = new Robot(kw, 0, 0, Direction.EAST);
+        Robot ay = new Robot(kw, 1, 0, Direction.EAST);
         
-        //Creating walls
-        new Thing(kw, 0, 1);
+        //Creating Track
+        new Wall(kw, 1, 0, Direction.SOUTH);
+        new Wall(kw, 1, 1, Direction.SOUTH);
+        new Wall(kw, 1, 2, Direction.SOUTH);
+        new Wall(kw, 1, 3, Direction.SOUTH);
+        new Wall(kw, 1, 4, Direction.SOUTH);
+        new Wall(kw, 1, 5, Direction.SOUTH);
+        new Wall(kw, 1, 6, Direction.SOUTH);
+        new Wall(kw, 1, 7, Direction.SOUTH);
+        new Wall(kw, 1, 8, Direction.SOUTH);
+        
+        //create hurdle
+        new Wall(kw, 1, 0, Direction.EAST);
+        new Wall(kw, 1, 1, Direction.EAST);
+        new Wall(kw, 1, 3, Direction.EAST);
+        new Wall(kw, 1, 6, Direction.EAST);
+        
+        //Create thing
+        new Thing(kw, 1, 8);
+        
+        //Move to end hurdles
+        
+        while(true){
+        //Check if it can move
+        //yes, move, no, turn over hurdle
+        //end if can pick up
+        //Check if bot can move
+        while (!ay.canPickThing()){
+        //Check if front is clear
+        while (ay.frontIsClear()){
+        //Move if clear
+        ay.move();
+        }
+        //Jump over hurdle
+        ay.turnLeft();
+        ay.move();
+        ay.turnLeft();
+        ay.turnLeft();
+        ay.turnLeft();
+        ay.move();
+        ay.turnLeft();
+        ay.turnLeft();
+        ay.turnLeft();
+        ay.move();
+        ay.turnLeft();
+        }
+        //Stop once the bot is on the bot
+        break;
+        }
+        
+      
     }
 }
