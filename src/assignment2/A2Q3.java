684d09f74d4d66e2d6d57e0d3b585238db78f965
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-02-16 10:13:09 -0500

    Initial Commit

--- src/assignment2/A2Q3.java
+++ src/assignment2/A2Q3.java
@@ -20,19 +20,50 @@ public class A2Q3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       // Create City
+        // Create City
         City kw = new City();
 
         // Create Bot
-        Robot ay = new Robot(kw, 1, 0, Direction.EAST);
-        
-       //Turn robot north
-while (ay.getDirection()) {
-   
-}      
-       //Keep moving north until on street 0
-       //Turn Robot west
-       //keep moving until on avenue 0
-           }
-           } 
-    
+        Robot ay = new Robot(kw, -6, 10, Direction.WEST);
+        //when it doesn't facwe east turn to face east
+        while (ay.getDirection() != Direction.EAST) {
+            ay.turnLeft();
+        }
+        //Turn robot north
+        //Check Street 
+        //move up if it is in negative avenue
+        if (ay.getStreet() < 0) {
+            ay.turnLeft();
+            ay.turnLeft();
+            ay.turnLeft();
+            while (ay.getStreet() < 0) {
+                ay.move();
+            }
+        }
+        //move down if it is in positive avenue
+        if (ay.getStreet() > 0) {
+            ay.turnLeft();
+            while (ay.getStreet() > 0) {
+                ay.move();
+            }
+        }
+        //move up if it is in negative avenue
+        if (ay.getAvenue() < 0) {
+            while (ay.getAvenue() < 0) {
+                ay.move();
+            }
+        }
+        //move down if it is in positive avenue
+        if (ay.getAvenue() > 0) {
+            ay.turnLeft();
+            ay.turnLeft();
+            ay.turnLeft();
+            while (ay.getAvenue() > 0) {
+                ay.move();
+            }
+        }
+    }
+    //Keep moving north until on street 0
+    //Turn Robot west
+    //keep moving until on avenue 0
+}
